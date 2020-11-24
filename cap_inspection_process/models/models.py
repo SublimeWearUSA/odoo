e33011fb685e9becafcd5831ee47b4d8eb63ee94
captivea-jbb <56834381+captivea-jbb@users.noreply.github.com>
2020-11-23 22:39:09 -0500

    Update models.py

--- cap_inspection_process/models/models.py
+++ cap_inspection_process/models/models.py
@@ -4,6 +4,15 @@ import logging
 import math
 from odoo import models, fields, api
 from datetime import date
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
 _logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
